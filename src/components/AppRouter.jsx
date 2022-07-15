40a8fddfdce22207471eb9c126f6fd8a44ab7b1d
AleksandrZn <leksandrzn@gmail.com>
2022-07-15 03:57:45 +0300

    Fix page display by incorrect link

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Routes, Route, Redirect } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { authRoutes, publicRoutes } from "../routes";
+import { MAIN_ROUTE } from "../utils/consts";
 
 const AppRouter = () => {
   const isAuth = true;
@@ -9,11 +10,12 @@ const AppRouter = () => {
       {isAuth &&
         authRoutes.map(({ path, Component }) => {
           console.log(path, Component);
-          return <Route key={path} path={path} element={<Component/>} exact />;
+          return <Route key={path} path={path} element={<Component />} exact />;
         })}
       {publicRoutes.map(({ path, Component }) => (
-        <Route key={path} path={path} element={<Component/>} exact />
+        <Route key={path} path={path} element={<Component />} exact />
       ))}
+      <Route path="*" element={<Navigate to={MAIN_ROUTE} replace />} />
     </Routes>
   );
 };
