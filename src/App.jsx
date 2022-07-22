6cdf01ef9d0d365b68f7db76615d4ebb8652088f
AleksandrZn <leksandrzn@gmail.com>
2022-07-22 06:14:31 +0300

    Add spiner

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./components/AppRouter";
+import { observer } from "mobx-react-lite";
+import { Context } from "./index";
+import { Spin, Alert } from "antd";
+import NavBar from "./components/NavBar";
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
 
-const App = () => {
+const App = observer(() => {
+  const { user } = useContext(Context);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [loading]);
+  
+  if (loading) {
+    return (
+      <Spin tip="Loading...">
+        <Alert
+          message="Alert message title"
+          description="Further details about the context of this alert."
+          type="info"
+        />
+      </Spin>
+    );
+  }
+
   return (
     <BrowserRouter>
+      <NavBar />
       <AppRouter />
     </BrowserRouter>
   );
-};
+});
 
 export default App;
